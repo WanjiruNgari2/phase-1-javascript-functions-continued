013f8668ca9a1036308e5ba4d8d66ddae8398efd
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-04 00:20:05 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// saturday fun
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; 
+}
+
+ 
+//monday work
+function mondayWork (activity = "go to the office") {
+return `This Monday, I will ${activity}.`;
+}
+
+
+//wrapadjective
+function wrapAdjective (wrapper = '*') {
+  return function (adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  }
+}
